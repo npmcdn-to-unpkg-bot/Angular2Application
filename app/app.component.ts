4f151c8b09a9251164791142e8f14dfd2350a144
Pankaj Kumar Dewangan <Pankajkumar.Dewangan@silicus.com>
2016-06-06 18:34:09 +0530

    Adding AsynchRoute

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,69 +6,70 @@ import { CardsComponent } from './card/cards.component';
 import { CardHistoryComponent } from './card/card.history.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, AsyncRoute, Router } from '@angular/router-deprecated';
 import { MissionService } from './mission.service';
-import { UserRegistrationComponent } from './login/user.registratoin.component';
+import { UserRegistrationComponent } from './login/user.registration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+declare var System:any;  
 
 @Component({
   selector: 'my-app',
-  templateUrl : `app/app.component.html`,
+  templateUrl: `app/app.component.html`,
   providers: [ROUTER_PROVIDERS, FirebaseService, LocalStorageService],
-  directives:[ROUTER_DIRECTIVES, CardsComponent, HomeComponent],
+  directives: [ROUTER_DIRECTIVES, CardsComponent, HomeComponent],
   inputs: ['flag']
 })
 @RouteConfig([
-    {
+  {
     path: '/',
     name: 'Home',
-    component: HomeComponent,
+    loader: () => System.import('./build/home.component').then(m => m.HomeComponent),   
     useAsDefault: true
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: UserRegistrationComponent    
-  },  
+    path: '/register',    
+    loader: () => System.import('./build/login/user.registration.component').then(m => m.UserRegistrationComponent),
+    name: 'Register'
+  },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: DashboardComponent    
+    loader: () => System.import('./build/dashboard/dashboard.component').then(m => m.DashboardComponent),
   },
   {
     path: '/history',
     name: 'History',
     component: CardHistoryComponent
   },
-    {
+  {
     path: '/cardHistoryDetailsComponent/:id',
     name: 'CardHistoryDetailsComponent',
     component: CardsComponent
   }
-  
+
 ])
-export class AppComponent  { 
+export class AppComponent {
   userName: string;
   password: string;
-  today:Date = new Date();
-  flag:boolean = false;
-  
+  today: Date = new Date();
+  flag: boolean = false;
+
   constructor(
-    private localstorage: LocalStorageService, 
+    private localstorage: LocalStorageService,
     private missionService: MissionService,
     private router: Router
-    ) {
+  ) {
     this.userName = this.localstorage.getStorage("userName");
 
     missionService.missionAnnounced$.subscribe(
       userName => {
         this.userName = userName;
         this.flag = true;
-      }); 
-  }  
-  
+      });
+  }
+
   logout() {
     this.localstorage.clear();
     this.flag = false;
     this.router.navigate(['Home']);
   }
-  
+
 }
