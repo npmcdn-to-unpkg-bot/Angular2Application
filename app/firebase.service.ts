17933db25ba1ea1ec62f3b39b18cfa8640868569
Pankaj Kumar Dewangan <pankaj.java12@gmail.com>
2016-06-21 14:54:06 +0530

    Commit prev codes

--- app/firebase.service.ts
+++ app/firebase.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
-import { da } from './../dashboard/dashboard.servce';
 
 @Injectable()
 export class FirebaseService {
